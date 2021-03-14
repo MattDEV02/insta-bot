ff9ab604a3a38d27f294bc65c05aad24b043a841
MattDEV02 <matteolambertucci3@gmail.com>
2021-03-14 11:59:53 +0100

    random interval

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ const trigger_click = elements => elements.map(el => el.click());
 
 const classes = ['.wpO6b  ', '.coreSpriteRightPaginationArrow'];
 
-const insta_bot = n => {
+const getRandom = (min = 2000,max = 4000) => {
+	const d = (max - min);
+	const r =  ((Math.random() * d) + min);
+	return r;
+};
+
+const insta_bot = () => { 
    let 
-      cuore = null , 
-      freccia = null;
+      like = null, 
+      arrow = null,
+	  r = getRandom();
    window.setInterval(() => { 
-      cuore = s(classes[0],1) ; 
-      freccia = s(classes[1]); 
-      trigger_click([cuore,freccia]);
-   }, n);
+	  like = s(classes[0],1) ; 
+      arrow = s(classes[1]); 
+      trigger_click([like, arrow]);
+	  r = getRandom();
+	  console.log(r);
+   }, r);
 };
 
 
-insta_bot(750); // You can change this.
+insta_bot(); 
