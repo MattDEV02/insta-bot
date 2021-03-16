c920179ef7aeca068ab82fc35de231b5152bbf1a
MatteDev02 <62772891+MattDEV02@users.noreply.github.com>
2021-03-16 18:18:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const trigger_click = elements => elements.map(el => el.click());
 
 const classes = ['.wpO6b  ', '.coreSpriteRightPaginationArrow'];
 
-const getRandom = (min = 2000,max = 4000) => {
+const getRandom = (min = 2500,max = 5000) => {
 	const d = (max - min);
 	const r =  ((Math.random() * d) + min);
 	return r;
@@ -13,16 +13,13 @@ const getRandom = (min = 2000,max = 4000) => {
 const insta_bot = () => { 
    let 
       like = null, 
-      arrow = null,
-	  r = getRandom();
+      arrow = null;
    window.setInterval(() => { 
 	  like = s(classes[0],1) ; 
-      arrow = s(classes[1]); 
-      trigger_click([like, arrow]);
-	  r = getRandom();
-	  console.log(r);
-   }, r);
+          arrow = s(classes[1]); 
+          trigger_click([like, arrow]);
+   }, getRandom());
 };
 
 
-insta_bot(); 
+//insta_bot(); 
